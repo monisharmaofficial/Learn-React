ff1f0bc28d04db301f9c4f99135db963d486ad07
monisharmaofficial <monika2022.sharma@gmail.com>
2024-07-28 14:15:39 +0530

    add new files

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
-//create element using js
-// const h2 = document.createElement('h2')
-// h2.innerText = 'Hello JS'
-// document.querySelector('#root').append(h2)
-
-// create element using react
-// const h2 = React.createElement('h2',{className:'subheading',id:'subtitle'}, 'Hello React')
-
-// const root = ReactDOM.createRoot(document.querySelector('#root'))
-
-// root.render(h2)
-
-
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 const container = React.createElement(
     'div',
@@ -22,12 +11,12 @@ const container = React.createElement(
         "Heading Element"
       ),
       React.createElement("section", {key : 1}, [
-        React.createElement("p",{}, "this is a paragraph"),
+        React.createElement("p",{}, "this is small paragraph"),
   
         React.createElement('img',{key:2, style:{width:'400px'}, src: 'https://images.pexels.com/photos/16720495/pexels-photo-16720495/free-photo-of-man-with-a-fishing-rod-on-a-shore-at-dusk.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'})
   
       ]),
-    ]
+    ] 
   );
   
   const root = ReactDOM.createRoot(document.querySelector('#root'))
