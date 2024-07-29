061dc717f703e1dd64b2130106a2bab44da45387
monisharmaofficial <monika2022.sharma@gmail.com>
2024-07-29 17:09:03 +0530

    01

--- Episode 01/App.js
+++ Episode 01/App.js
@@ -1,18 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [ React.createElement("div",{id:"child1"},
-            [ React.createElement("h1",{},"i am an nested h1 element"),
-                React.createElement("h2",{},"i am an nested h2 element")
-            ]),
-        React.createElement("div",{id:"child2"},
-            [ React.createElement("h1",{},"i am an nested h1 element"),
-                React.createElement("h2",{},"i am an nested h2 element")
-            ])
+const container = React.createElement(
+    'div',
+    { className: 'container', id: 'container' },
+    [
+      React.createElement('section', { key: 1 }, [
+        React.createElement(
+          'p',
+          { key: 1 },
+          'The library for web and native user interfaces'
+        ),
+        React.createElement('img', {
+          key: 2,
+          style: {
+            width: 200,
+            backgroundColor: 'teal',
+            borderRadius: 8,
+            padding: 16,
+          },
+          src: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+        }),
+      ]),
+      React.createElement('section', { key: 2 }, [
+        React.createElement('form', { key: 1 }, [
+          React.createElement('div', {className: 'input-group', key: 1}, [
+            React.createElement(
+              'label',
+              { key: 1, htmlFor: 'username' },
+              'Username'
+            ),
+            React.createElement('input', { key: 2, id: 'username' }),
+          ]),
+          React.createElement('div', {className: 'input-group', key: 2}, [
+            React.createElement(
+              'label',
+              { key: 1, htmlFor: 'password' },
+              'Password'
+            ),
+            React.createElement('input', { key: 2, id: 'password', type: 'password' }),
+          ]),
+        ]),
+      ]),
     ]
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+  )
+  
+  const root = ReactDOM.createRoot(document.querySelector('#root'))
+  
+  root.render(container)
